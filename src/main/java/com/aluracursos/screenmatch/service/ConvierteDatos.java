c19faf148f5f20ff65e76d27378200bb1fc30d71
Jorge Martinez <tnlcomputer@gmail.com>
2025-11-25 10:11:54 -0300

    screenmatch spring

--- src/main/java/com/aluracursos/screenmatch/service/ConvierteDatos.java
+++ src/main/java/com/aluracursos/screenmatch/service/ConvierteDatos.java
@@ -6,7 +6,6 @@ import tools.jackson.databind.ObjectMapper;
 public class ConvierteDatos implements IConvierteDatos {
     private ObjectMapper objectMapper = new ObjectMapper();
 
-
     @Override
     public <T> T obtenerDatos(String json, Class<T> clase) {
         try {
